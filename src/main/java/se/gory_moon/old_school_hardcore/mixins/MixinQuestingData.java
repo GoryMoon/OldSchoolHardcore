a112dc30962138d816ac92fc9b2c9e37b612a99e
GoryMoon <gurreja@gmail.com>
2023-06-15 01:27:52 +0200

    feat: updated to 1.19.3

--- src/main/java/se/gory_moon/old_school_hardcore/mixins/MixinQuestingData.java
+++ src/main/java/se/gory_moon/old_school_hardcore/mixins/MixinQuestingData.java
@@ -1,16 +1,17 @@
 package se.gory_moon.old_school_hardcore.mixins;
 
-import hardcorequesting.common.forge.quests.QuestingData;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import se.gory_moon.old_school_hardcore.utils.CommonUtils;
 import se.gory_moon.old_school_hardcore.Config;
 
-@Mixin(QuestingData.class)
+@Pseudo
+@Mixin(targets = "hardcorequesting.common.forge.quests.QuestingData")
 public class MixinQuestingData {
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;displayClientMessage(Lnet/minecraft/network/chat/Component;Z)V", remap = true), method = "outOfLives", remap = false, cancellable = true)
